a75856028719799bb7ad8e1baa0a276c4c9b08f4
J.H Park <blogerhong@naver.com>
2024-11-01 10:18:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        
+        int result = 0;
+        
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i] == false) {
+                absolutes[i] *= -1;
+            }
+            
+            result += absolutes[i];
+        }
+        
+        return result;
+    }
+}
