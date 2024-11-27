6216c2f532a0f28c78bb80fdf19b1bc40aa3f1c4
J.H Park <blogerhong@naver.com>
2024-11-27 12:06:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < words.length; i++) {
+            if (s.contains(words[i])) {
+                s = s.replace(words[i], Integer.toString(i));
+            }
+        }
+        
+        return Integer.parseInt(s);
+    }
+}
